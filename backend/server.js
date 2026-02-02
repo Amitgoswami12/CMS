24ed9280d1cd20b0a31a5eb6e5779e3b8cc66af6
Amitgoswami12 <amitgoswami698@gmail.com>
2026-02-02 17:22:41 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,11 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*",
+  }
+));
 app.use(express.json({ limit: "50mb" }));
 
 app.get(`/`, (req, res) => {
